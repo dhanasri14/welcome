cb19407f486aa7019ed1f916aca371a16504ce5a
Nandhakumar <Nandhakumar@LAPTOP-7QM6QFOR>
2023-08-03 22:05:48 +0530

    addition

---
+++ welcome/src/welcomeproject/demo.java
@@ -0,0 +1,12 @@
+package welcomeproject;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubint low = 999, high = 99999;
+
+		int n1 = 225, n2 = 115, sum;  
+		sum = n1 + n2;  
+		System.out.println("The sum of numbers is: "+sum);  
+		}  
+		}  
