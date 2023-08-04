d69e385b740b2fcdab56ad12f407434f73083a5c
Nandhakumar <Nandhakumar@LAPTOP-7QM6QFOR>
2023-08-04 21:07:13 +0530

    subtraction

---
+++ welcome/src/welcomeproject/string.java
@@ -0,0 +1,21 @@
+package welcomeproject;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=50;
+		int y=10;
+
+		// declare another variable to hold the substracted value
+		int subtract;
+
+		// calculate the substraction 
+		subtract = x - y;
+
+		// print the calculated subtract value
+		System.out.println("The substraction of two Numbers: " + subtract);
+
+	}
+
+}
