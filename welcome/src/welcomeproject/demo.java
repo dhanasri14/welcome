6ae9639463fcc3803e178ffb6f89e9b0bd85610a
Nandhakumar <Nandhakumar@LAPTOP-7QM6QFOR>
2023-08-04 17:18:31 +0530

    char project

--- welcome/src/welcomeproject/demo.java
+++ welcome/src/welcomeproject/demo.java
@@ -4,9 +4,20 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubint low = 999, high = 99999;
-
-		int n1 = 225, n2 = 115, sum;  
-		sum = n1 + n2;  
-		System.out.println("The sum of numbers is: "+sum);  
-		}  
-		}  
+		char ch = 'a';
+		System.out.println(ch);
+		// assigning number to char
+		char ch1 = 65;
+		System.out.println(ch1);
+		// assigning unicode to char
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		// Unicode representation
+		char var2 = '\u20AC';
+		System.out.println(var2);
+		// ASCI code representation
+		int a = 'A';
+		System.out.println(a);
+		}
+		}
+		
